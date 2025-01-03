c537f9a56e581afeb5448e70cc29354995da6196
GoddoNebianU <goddonebianu@outlook.com>
2025-01-03 10:00:02 +0800

    增加正确率显示，修改了答题逻辑

--- index.js
+++ index.js
@@ -13,17 +13,21 @@ const KEY_MAP = {
 };
 
 class MeiYu {
-    constructor () {
+    constructor() {
         this.meiyuinfo = null;
         this.options = null;
         this.meiyu = [];
         this.currentItem = null;
+        this.history = [];
+        this.pause = false;
+        this.correctRate = null;
     }
 
-    async init (){
+    async init() {
         this.meiyuinfo = document.getElementById("meiyuinfo");
         this.meiyuinfo = document.getElementById("meiyuinfo");
         this.options = Array.from(document.getElementsByClassName("option"));
+        this.correctRate = document.getElementById("correctRate");
 
         const response = await fetch("./meiyu.json");
         if (!response.ok) throw new Error(`HTTP error! status: ${response.status}`);
@@ -34,11 +38,28 @@ class MeiYu {
         this.showWelcomeAnswer();
     }
 
-    showWelcomeAnswer () {
+    record(rcd) {
+        this.history.push(rcd);
+        if (this.history.length > 40) {
+            this.history.shift();
+        }
+        this.correctRate.innerText = `（正确率：${
+            (() => {
+                const total = this.history.length;
+                let right = 0;
+                for (let i = 0; i < total; i++) {
+                    if (this.history[i][1]) right++;
+                }
+                return (100 * right / total).toFixed(2) + "%";
+            })()
+        }）`;
+    }
+
+    showWelcomeAnswer() {
         M.toast({html: "（可以用数字键盘作答）"})
     }
 
-    bindEvents () {
+    bindEvents() {
         document.addEventListener("keypress", (event) => {
             const optionIndex = KEY_MAP[event.key];
             if (optionIndex !== undefined) {
@@ -52,21 +73,35 @@ class MeiYu {
         });
     }
 
-    handleAnswer (answer) {
+    handleAnswer(answer) {
+        if (this.pause) return;
+
         if (this.currentItem.answer === answer) {
             M.toast({html: "Correct!"});
+            this.record([this.currentItem.index, true]);
             this.renewItem();
         } else {
+            this.pause = true;
             M.toast({
-                html: "Incorrect! Answer: " + this.currentItem["options"][ANSWER_MAP[this.currentItem['answer']]]
+                html: "Incorrect! Answer: " + this.currentItem["options"][ANSWER_MAP[this.currentItem['answer']]],
+                classes: "red"
             });
+            this.record([this.currentItem.index, false]);
+            setTimeout(() => {
+                this.renewItem();
+                this.pause = false;
+            }, 5000);
         }
     }
 
-    renewItem () {
-        const i = Math.floor(Math.random() * this.meiyu.length);
+    renewItem() {
+        let i;
+        do {
+            i = Math.floor(Math.random() * this.meiyu.length);
+        } while (this.history.some((pair) => pair[0] === i));
         this.currentItem = this.meiyu[i];
-        this.meiyuinfo.innerText = `${i+1}. ` + this.currentItem["question"];
+        this.currentItem.index = i;
+        this.meiyuinfo.innerText = `${i + 1}. ` + this.currentItem["question"];
         this.options.forEach((option, index) => {
             option.innerText = "ABCD"[index] + ". " + this.currentItem.options[index];
         });
